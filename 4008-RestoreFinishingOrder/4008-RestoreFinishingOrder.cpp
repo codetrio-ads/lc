79fa371c96ecadb2b0e25e2a9a0d4b813151b821
Kumar Satish Kabra <blastercoder01@gmail.com>
2026-08-09 00:37:55 +0530

    Create 4008-RestoreFinishingOrder

---
+++ 4008-RestoreFinishingOrder/4008-RestoreFinishingOrder.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/8/2026, 12:37:52 am
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        vector<int> arr;
+        for(int i=0;i<order.size();i++){
+            for(int j=0;j<friends.size();j++){
+                if(order[i]==friends[j]){
+                    arr.push_back(order[i]);
+                }
+            }
+        }
+        return arr;
+    }
+};
