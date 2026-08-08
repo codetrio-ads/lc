cf438e4aff07e7b43a935cd6a45d91fcc0f4ffc9
Kumar Satish Kabra <blastercoder01@gmail.com>
2026-08-09 00:38:37 +0530

    Create 347-TopKFrequentElements

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.cpp
@@ -0,0 +1,25 @@
+// Last updated: 9/8/2026, 12:38:34 am
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int, int> freq;
+for (int num : nums) {
+    freq[num]++;
+}
+
+vector<vector<int>> buckets(nums.size() + 1);
+for (auto &p : freq) {
+    buckets[p.second].push_back(p.first);
+}
+
+vector<int> result;
+for (int i = buckets.size() - 1; i >= 0 && result.size() < k; i--) {
+    for (int num : buckets[i]) {
+        result.push_back(num);
+        if (result.size() == k) break;
+    }
+}
+
+return result;
+    }
+};
